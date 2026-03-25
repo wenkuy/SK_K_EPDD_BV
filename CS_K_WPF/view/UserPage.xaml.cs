5fd6a07f62ddf86bb1be3d169974272c7343bf7f
doublek <2206221361@qq.com>
2026-03-25 20:47:58 +0800

    实现了动画。 1.在按钮下直接实现 2.另一种，通过雪灾顶级容器的triggers集合内，统一管理的

--- CS_K_WPF/view/UserPage.xaml.cs
+++ CS_K_WPF/view/UserPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,10 @@ namespace CS_K_WPF.view
         public UserPage()
         {
             InitializeComponent();
+            DoubleAnimation doubleAnimation = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(0.5)); // 从0到1，持续0.5秒的动画
+            doubleAnimation.Duration = new Duration(TimeSpan.FromSeconds(0.5)); // 设置动画持续时间
+            doubleAnimation.BeginTime = TimeSpan.FromSeconds(0); // 设置动画开始时间
+            new Button().BeginAnimation(OpacityProperty, doubleAnimation); // 对Button1的Opacity属性应用动画
         }
     }
 }
